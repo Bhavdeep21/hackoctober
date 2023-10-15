68979d7ffb69227aac92db56ba185f632691cfd7
Bhavdeep Singh <55994813+Bhavdeep21@users.noreply.github.com>
2023-10-15 17:55:19 +0530

    Update all the paths from one node in grapy SQL.sql

--- all the paths from one node in grapy SQL.sql
+++ all the paths from one node in grapy SQL.sql
@@ -1,7 +1,7 @@
 //Finding all the paths from one node to another in a graph:
 
 //1. Create a CTE called paths that contains the paths from node 1 to all the nodes that are directly connected to it.
-//2. Recursively add the paths from the end nodes of the existing paths to the CTE by joining the paths CTE with the graph table //and selecting the end_node column.
+//2. Recursively add the paths from the end nodes of the existing paths to the CTE by joining the paths CTE with the graph table and selecting the end_node column.
 //3. Select the paths from the paths CTE where the end node is node 2.
 WITH recursive paths AS (
   SELECT start_node, end_node
